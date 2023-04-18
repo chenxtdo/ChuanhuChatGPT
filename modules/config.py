d3326f11a76732b28d3a67590eb183e1c74f67c3
icbbetter <chenxtdo@gmail.com>
2023-04-18 19:21:16 +0800

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -31,8 +31,8 @@ if os.path.exists("config.json"):
         config = json.load(f)
 else:
     config = {
-        server_port: int(os.environ.get('PORT')),
-        openai_api_key: os.environ.get('OPENAI_API_KEY'),
+        'server_port': os.environ.get('PORT'),
+        'openai_api_key': os.environ.get('OPENAI_API_KEY'),
     }
 
 language = config.get("language", "auto")
